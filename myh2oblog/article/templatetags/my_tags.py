569812ba7e51277d5d0a89f11f368a92141e6efd
FQY <qingyun-feng@qq.com>
2022-02-01 16:13:40 +0800

    auto commit at 2022-02-01 16:13:39

--- myh2oblog/article/templatetags/my_tags.py
+++ myh2oblog/article/templatetags/my_tags.py
@@ -3,6 +3,7 @@
 # @Author : 风轻云
 # @File : my_tags.py
 from django import template
+from article.models import Tags, Categories, Articles
 
 register = template.Library()
 
@@ -17,3 +18,17 @@ def cut(value, change):
 def lower(value):  # Only one argument.
     """Converts a string into all lowercase"""
     return value.lower()
+
+
+@register.inclusion_tag('my_tags/tag_cloud.html')
+def tag_cloud():
+    tags = Tags.objects.all()
+    return locals()
+
+
+@register.inclusion_tag('my_tags/category_cloud.html')
+def category_cloud():
+    categories = Categories.objects.all()
+    for category in categories:
+        category.number = Articles.objects.filter(category=category).count()
+    return locals()
